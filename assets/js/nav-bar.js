1e0218f78bde7d4de185a65a09c960ab3d83faa9
Kim Gwang woon <misili304@naver.com>
2023-05-15 09:51:39 +0900

    Feat/ Random Logos

--- assets/js/nav-bar.js
+++ assets/js/nav-bar.js
@@ -1,15 +1,12 @@
 window.addEventListener("load", function () {
-    var header = document.querySelector('#header');
+    var header = document.querySelector('#logo');
     console.log(header);
     const max = 7;
     const randomInt = Math.floor(Math.random() * (max)) + 1;
     console.log(randomInt)
-    header.innerHTML += `<div id="logo">
-                            <h1 class="afasfsa">
+    header.innerHTML = `<h1 class="afasfsa">
                                 <a class="logobox" href="index.html">
                                     <img src="images/logos/logo${randomInt}.png" alt="">
                                 </a>
-                            </h1>
-                            <span>by 가보장고</span>
-                        </div>`
+                            </h1>`
 });
